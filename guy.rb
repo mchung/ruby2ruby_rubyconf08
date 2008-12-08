265e0d33fac16c42d038cb41ea4520d8c93548ca
Marc Chung <mchung@gmail.com>
2008-12-08 13:37:07 -0700

    guy.rb comments

--- guy.rb
+++ guy.rb
@@ -1,3 +1,12 @@
+# A small tribute to Guy Decoux, an early Ruby programmer who once walked the 
+# Ruby parse tree to answer a simple operator precedence question posed by Matz.
+
+# Read Matz’s question [1] and Guy’s [2] answer.  Today, we can answer the same 
+# question with the following code:
+
+# [1] http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/2462
+# [2] http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/2464
+
 if $0 == __FILE__
   require 'rubygems'
   require 'parse_tree'
